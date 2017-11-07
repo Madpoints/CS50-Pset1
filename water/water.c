78cae6df88e14005c7d81e6252b0f30dc43c783d
John Furtado <jf8846@gmail.com>
2017-11-07 00:39:11 +0000

    CS50 styling

--- water/water.c
+++ water/water.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main(void) {
+int main(void)
+{
+    // get # of minutes user intends to shower for
     printf("minutes: ");
     int m = get_int();
-
+    // get amount of water used per minute
     int t = (m * 192);
-
+    // get # of bottles of water
     int b = (t / 16);
-
+    // return # of bottles used
     printf("bottles: %i\n", b);
-
 }
